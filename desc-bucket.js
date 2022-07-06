0c28e939b3e60dc45a3d2869069f32c5e49a6dba
Ojas Mahara <88713256+OjasWithGithub@users.noreply.github.com>
2022-07-06 15:33:43 +0530

    Add files via upload

---
+++ desc-bucket.js
@@ -0,0 +1,56 @@
+status = ""
+objects = []
+function preload(){
+
+    img = loadImage("desc-bucket.jpg")
+}
+
+function setup(){
+    canvas = createCanvas(550,420);
+    canvas.center()
+
+
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML = "Model Loading..."
+}
+
+function modelLoaded(){
+    document.getElementById("status").innerHTML = "Model Loaded!"
+    console.log("Model_loaded")
+    status = true
+    objectDetector.detect(img,gotResult)
+
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects = results
+}
+
+function draw(){
+
+    image(img,0,0,550,420)
+    
+    if (status != ""){
+        for (i = 0; i < objects.length; i++){
+
+            r = random(255)
+            g = random(255)
+            b = random(255)
+
+            document.getElementById("objects").innerHTML = "No.of Objects: 2| No.of objects detected: "+objects.length;
+
+            fill(r,g,b)
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " "+percent+"%", objects[i].x + 15,objects[i].y + 15)
+            noFill();
+            stroke(r,g,b)
+            rect(objects[i].x, objects[i].y,objects[i].width,objects[i].height)
+ 
+        }
+    }
+
+}
